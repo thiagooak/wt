0b3bd3102a384847d2b41dbdee744a0f3b613a6f
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-17 11:09:28 +0200

    HTML support
    Fix #1
    
    Ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use structopt::StructOpt;
 
 /// Writing Tools
@@ -6,14 +7,56 @@ struct Cli {
     /// The path to the file you want to check
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+    /// HTML mode
+    #[structopt(short = "H")]
+    html: bool,
 }
 
 fn main() {
     let args = Cli::from_args();
 
-    let content = std::fs::read_to_string(&args.path).unwrap();
+    let raw = std::fs::read_to_string(&args.path).unwrap();
+    let content = prepare_content(raw, args.html);
 
     println!("{}", check_sentence_length(content));
+}
+
+fn prepare_content(raw: String, html: bool) -> String {
+    if html {
+        // replacing every html tag with a period
+        // this is very wrong but is a simple way to get strated
+        // ideally this would convert some tags to periods </p></div> (e.g. tags that by default are blocks)
+        // and replace other with nothing </strong></a>
+        let re = Regex::new(r"(<[^>]*>)").unwrap();
+        return re.replace_all(&raw, ".").into_owned(); // I don't understand what into_owned does! But this works
+    }
+
+    return raw;
+}
+
+#[test]
+fn test_prepare_content() {
+    let content = String::from(
+        "<!doctype html>
+<html class=\"no-js\" lang=\"\">
+
+<head><meta charset=\"utf-8\"><title></title></head>
+
+<body>
+    <!-- Add your site or application content here -->
+    <p>Hello world! This is HTML5 Boilerplate.</p>
+    <script src=\"js/main.js\"></script>
+</body>
+</html>
+",
+    );
+
+    assert_eq!(
+        prepare_content(content.clone(), true),
+        String::from(".\n.\n\n.....\n\n.\n    .\n    .Hello world! This is HTML5 Boilerplate..\n    ..\n.\n.\n")
+    );
+
+    assert_eq!(prepare_content(content.clone(), false), content.clone());
 }
 
 fn check_sentence_length(content: String) -> String {
@@ -35,7 +78,8 @@ fn check_sentence_length(content: String) -> String {
 
     let long_sentences_count: f64 = long_sentences.len() as f64;
 
-    let percent_long_sentences: f64 =  long_sentences_count / (short_sentences_count + long_sentences_count) * 100.0;
+    let percent_long_sentences: f64 =
+        long_sentences_count / (short_sentences_count + long_sentences_count) * 100.0;
 
     if percent_long_sentences > 10.0 {
         result.push_str("\nMore than 10% of your sentences have > 18 words");
