6975883d5eaa205be8370c80886f881ac268ce5e
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-17 09:34:58 +0200

    Simpler output. Add tests.
    Fix #4

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # wt
-Writing Tools. A command-line style checker for technical writers. Mostly me. 😅
+Writing Tools. A command-line style checker for technical writers.
+
+## Usage
+
+```
+wt example.txt
+
+More than 10% of your sentences have > 18 words
+    Vivamus lacinia, libero ac lobortis iaculis, dui dui malesuada diam, e...
+```
+
+example.txt
+```
+Vivamus lacinia, libero ac lobortis iaculis, dui dui malesuada diam, eu interdum tellus risus nec leo class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+Pellentesque vel elit maximus, suscipit turpis a, elementum turpis.
+
+Vivamus sollicitudin arcu sit amet elementum fermentum.
+Vestibulum sed velit in dolor molestie congue.
+
+Vestibulum dui quam, pharetra non egestas id, ullamcorper et mauris.
+
+Vestibulum blandit felis quis ligula finibus commodo.
+```
+
+## Installation
+
+Download the latest [MacOS binary](https://github.com/thiagooak/wt/releases).
+
+e.g.
+
+```
+curl -fsSLO https://github.com/thiagooak/wt/releases/download/v0.1.1/wt
+
+mv wt /usr/local/bin && chmod +x /usr/local/bin/wt
+
+wt --help
+```
+
+or build it
+```
+git clone git@github.com:thiagooak/wt.git
+cd wt
+
+cargo build --release
+
+./target/release/wt --help
+```
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,13 @@ fn main() {
 
     let content = std::fs::read_to_string(&args.path).unwrap();
 
+    println!("{}", check_sentence_length(content));
+}
+
+fn check_sentence_length(content: String) -> String {
     let sentences: Vec<&str> = content.split_terminator('.').collect();
 
+    let mut result = String::new();
     let mut short_sentences_count: f64 = 0.0;
     let mut long_sentences = Vec::new();
 
@@ -33,15 +38,26 @@ fn main() {
     let percent_long_sentences: f64 =  long_sentences_count / (short_sentences_count + long_sentences_count) * 100.0;
 
     if percent_long_sentences > 10.0 {
-        println!("\nMore than 10% of your sentences have > 18 words.");
+        result.push_str("\nMore than 10% of your sentences have > 18 words");
         for sentence in long_sentences {
-            println!("\t{}\n", sentence);
+            result.push_str("\n\t");
+            result.push_str(&sentence[..70]);
+            result.push_str("...");
         }
     }
 
-    println!("\n{} short sentences and {} long ones. {:.1}%",
-        short_sentences_count,
-        long_sentences_count,
-        percent_long_sentences
+    return result;
+}
+
+#[test]
+fn test_check_sentence_length() {
+    assert_eq!(
+        check_sentence_length(String::from("Vivamus lacinia, libero ac lobortis iaculis, dui dui malesuada diam, eu interdum tellus risus nec leo class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque vel elit maximus, suscipit turpis a, elementum turpis. Vivamus sollicitudin arcu sit amet elementum fermentum. Vestibulum sed velit in dolor molestie congue. Vestibulum dui quam, pharetra non egestas id, ullamcorper et mauris. Vestibulum blandit felis quis ligula finibus commodo.")),
+        String::from("\nMore than 10% of your sentences have > 18 words\n\tVivamus lacinia, libero ac lobortis iaculis, dui dui malesuada diam, e...")
+    );
+
+    assert_eq!(
+        check_sentence_length(String::from("Vivamus sollicitudin arcu sit amet elementum fermentum. Vestibulum sed velit in dolor molestie congue. Vestibulum dui quam, pharetra non egestas id, ullamcorper et mauris. Vestibulum blandit felis quis ligula finibus commodo.")),
+        String::from("")
     );
 }
